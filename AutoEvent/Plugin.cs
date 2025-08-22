ae95720d0a1e526e659618b680a6ec385c4ac8dc
MedveMarci <martonmende@gmail.com>
2025-08-22 17:26:46 +0200

    Version fix and renamed the AssemblyName

--- AutoEvent/Plugin.cs
+++ AutoEvent/Plugin.cs
@@ -25,7 +25,7 @@ public abstract class AutoEvent : Plugin<Config>
     public override string Description =>
         "A plugin that allows you to play mini-games in SCP:SL. It includes a variety of games such as Spleef, Lava, Hide and Seek, Knives, and more. Each game has its own unique mechanics and rules, providing a fun and engaging experience for players.";
 
-    public override Version Version => new(10, 0, 0, 0);
+    public override Version Version => new(9, 14, 0, 0);
     public override Version RequiredApiVersion => new(LabApiProperties.CompiledVersion);
 
     public static string BaseConfigPath { get; private set; }
@@ -83,6 +83,7 @@ public abstract class AutoEvent : Plugin<Config>
             CustomHandlersManager.RegisterEventsHandler(EventHandler);
 
             LogManager.Info("The mini-games are loaded.");
+            LogManager.Warn("This is a pre-release version. There might be bugs, if you find one, please report it on GitHub or Discord.");
         }
         catch (Exception e)
         {
