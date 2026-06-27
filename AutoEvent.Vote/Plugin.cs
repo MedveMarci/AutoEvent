1b53d0e4412673a1cc3d9c1035b3ccfd6e10fe12
MedveMarci <martonmende@gmail.com>
2026-06-27 16:55:09 +0200

    Version bump

--- AutoEvent.Vote/Plugin.cs
+++ AutoEvent.Vote/Plugin.cs
@@ -14,7 +14,7 @@ public class AutoEventVote : Plugin<Config>
     public override string Name => "AutoEvent.Vote";
     public override string Description => "VoteSystem module for AutoEvent";
     public override string Author => "MedveMarci";
-    public override Version Version { get; } = new(1, 0, 2);
+    public override Version Version { get; } = new(1, 0, 3);
     public override Version RequiredApiVersion { get; } = new(LabApiProperties.CompiledVersion);
     internal static AutoEventVote Singleton { get; private set; }
 
